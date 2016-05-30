c9b62cbdac8ef7f00025abf0edb37d853ae7cf0c
Andrew Broekman <andrewbroekman@gmail.com>
2016-05-30 22:40:32 +0200

    Fixes unit testing error in application component spec

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,17 +1,17 @@
-import {
-  it,
-  inject,
-  describe,
-  beforeEachProviders,
-  expect
-} from '@angular/core/testing';
+import {Router, ROUTER_PROVIDERS} from "@angular/router";
+
 import { AppComponent } from './app.component';
+
 describe('App', () => {
-  beforeEachProviders(() => [
-    AppComponent
-  ]);
-  it ('should work', inject([AppComponent], (app: AppComponent) => {
-    // Add real test here
-    expect(2).toBe(2);
-  }));
+  let component: AppComponent;
+  let router: Router;
+  
+  beforeAll(() => {
+    router = jasmine.createSpyObj("Router", ['navigate']);
+    component = new AppComponent(router);
+  });
+  
+  it ('should be defined', () => {
+    expect(component).toBeDefined();
+  });
 });
