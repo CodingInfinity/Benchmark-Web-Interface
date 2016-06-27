86c3c8f2e114570c1f46305130786d0fabe26716
Fabio Loreggian <u14040426@tuks.co.za>
2016-06-27 17:54:46 +0200

    Can edit first name and last name and email, but request to server is not working, will fix at a later stage

--- src/app/components/account/profile/profile.component.ts
+++ src/app/components/account/profile/profile.component.ts
@@ -4,7 +4,8 @@ import { MaterializeDirective } from "angular2-materialize";
 import {NavigationComponent} from "../../navigation/navigation.component";
 import {FooterComponent} from "../../footer/footer.component";
 import {SecureComponent} from "../../../services/secure.component";
-import {Client} from "../../../services/api.service";
+import {Client,UpdateUserRequest} from "../../../services/api.service";
+import {ControlGroup, FormBuilder, Validators} from "@angular/common";
 
 
 
@@ -20,22 +21,85 @@ import {Client} from "../../../services/api.service";
 })
 export class ProfileComponent extends SecureComponent {
 
-    constructor(router:Router, private client: Client) {
+    private firstname: string;
+    private lastname: string;
+    private email: string;
+    private nameChange: boolean = false;
+    private emailChange: boolean = false;
+
+    private nameForm: ControlGroup;
+    private emailForm: ControlGroup;
+
+
+    constructor(router:Router, private client: Client, private fb: FormBuilder) {
       super(router);
       this.authorities = ["ROLE_ADMIN", "ROLE_USER"];
+      this.nameForm = fb.group({
+        firstName: ['', Validators.required],
+        lastName: ['', Validators.required]
+      });
+      this.emailForm = fb.group({
+        email: ['', Validators.required]
+      });
     }
 
     routerOnActivate(curr:RouteSegment, prev?:RouteSegment, currTree?:RouteTree, prevTree?:RouteTree):void{
       super.routerOnActivate(curr, prev, currTree, prevTree);
       //Get the use profile from the server
-      this.client.getAccountUsingGET().subscribe((response)=>{
-        this.hasError =false;
-        console.log(response);
-      },(err)=>{
-        console.log(err);
-        this.errorMessage = err.json()["message"];
-        this.hasError = true;
-        this.showMessage = false;
-      });
+      this.firstname =  JSON.parse(localStorage.getItem("user_token"))["firstName"];
+      this.lastname =  JSON.parse(localStorage.getItem("user_token"))["lastName"];
+      this.email =  JSON.parse(localStorage.getItem("user_token"))["email"];
+    }
+
+    editNameSelect(){
+      this.nameChange = true;
+      this.emailChange = false;
+    }
+
+    editEmailSelect(){
+      this.nameChange = false;
+      this.emailChange = true;
+    }
+
+    editName(value: any){
+      this.nameChange = false;
+      if(this.firstname != value.firstname){
+        this.firstname = value.firstname;
+      }
+
+      if(this.lastname != value.lastname){
+        this.lastname = value.lastname;
+      }
+      this.sendUpdateRequest();
+    }
+
+    editEmail(value:any){
+      console.log(value);
+      this.emailChange = false;
+      if(this.email != value.email){
+        this.email = value.email;
+      }
+      this.sendUpdateRequest();
+    }
+
+    sendUpdateRequest(){
+      var updatedUser: UpdateUserRequest = {
+        email: this.email,
+        firstName: this.firstname,
+        lastName: this.lastname
+      }
+
+      this.client.updateUserUsingPUT(updatedUser).subscribe(
+        (response)=>{
+          this.showMessage = true;
+          this.hasError = false;
+          this.message = "You have successfully edited your profile!";
+        },
+        (err)=>{
+          console.log(err.json());
+          this.errorMessage = err.json()["message"];
+          this.hasError = true;
+          this.showMessage = false;
+        });
     }
 }
