73223a96f496c3e5522b8fa1a516203a18b80df8
Fabio Loreggian <u14040426@tuks.co.za>
2016-08-23 20:18:17 +0200

    Changed the theme, hope you all like

--- src/app/components/navigation/navigation.component.ts
+++ src/app/components/navigation/navigation.component.ts
@@ -1,14 +1,27 @@
-import { Component } from "@angular/core";
+import {Component, OnInit} from "@angular/core";
 import {APIService} from "../../services/api.service";
 
 @Component({
   selector: "navigation",
   template: require('./navigation.component.html'),
   styles: [require('./navigation.component.css')],
+  inputs:["heading"]
 })
-export class NavigationComponent{
+export class NavigationComponent implements OnInit{
   private isAdmin: boolean = false;
-  constructor(private client: APIService){
+  private loggedIn: boolean = false;
+  private username: string = "";
+  constructor(private client: APIService, private api: APIService){
     this.isAdmin = this.client.hasRole("ROLE_ADMIN");
+  }
+
+  ngOnInit(){
+    console.log("Hello");
+    this.loggedIn = this.api.authenticated();
+    console.log(this.loggedIn);
+    if(this.loggedIn){
+      this.username = JSON.parse(localStorage.getItem("user_token"))["username"];
+      console.log(this.username);
+    }
   }
 }
